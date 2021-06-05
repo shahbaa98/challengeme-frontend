4345aeb7eab7b9690af3c93c4ecf09e1ca86a3a2
mayada0bahij <https://dbcourse@mayada0bahij-technion.scm.azurewebsites.net:443/mayada0bahij-technion.git>
2021-06-05 17:04:04 +0300

    test 1

--- src/components/TeacherLogin.js
+++ src/components/TeacherLogin.js
@@ -1,20 +1,51 @@
-import React, { useContext } from "react";
-import { useState } from "react";
-
-import {
-  BoldLink,
-  BoxContainer,
-  FormContainer,
-  Input,
-  MutedLink,
-  SubmitButton,
-} from "./Common";
-import { Marginer } from "./Marginer";
-import { AccountContext } from "./AccountContext";
 import { useHistory } from 'react-router-dom';
+import img from './imgs/image3.png'
+
+import './style.css'
+const Teacherlogin = () =>{
+    const history = useHistory();
+
+    return(
+        alert = () => {
+            return(<h1>הודעה נשלחה למורה שלך, אל דאגה הסיסמה שלך תשלח אליך בקרוב</h1>);
+        },
+            <div className="app-com">
+                <div>
+                    <img align="left" src={img} alt="" width="200" height="200"/>
+                </div>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <h1>
+                    התחבר
+                </h1>
+                <div >
+                    <input className="placeholder" type="text" placeholder="מספר טלפון"/>
+                </div>
+                <br/>
+                <div >
+                    <input className="placeholder" type="text" placeholder="סיסמה"/>
+                </div>
+                <text onPress={()=>{}}>
+                    שכחת סיסמה?
+                </text>
+                <div >
+                    <button className="buttonStyle" onClick={()=> {history.push('/StudentProfile')}}>
+                        התחבר
+                    </button>
+                </div>
+                <text onPress={()=>{}}>
+                    אין לך חישבון ?הרשמה
+                </text>
+
+            </div>
+    )
+}
 
 
-export function Teacherlogin(props) {
+/*export function Teacherlogin(props) {
     const history = useHistory();
     const [alignment, setAlignment] = useState("right");
 
@@ -38,6 +69,6 @@ export function Teacherlogin(props) {
       </MutedLink>
     </BoxContainer>
   );
-}
+}*/
 export default Teacherlogin 
 
