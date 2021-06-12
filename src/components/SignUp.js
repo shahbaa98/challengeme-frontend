81ee34688fe1c221dd09f231088eaebc0765421b
mayada0bahij <https://dbcourse@mayada0bahij-technion.scm.azurewebsites.net:443/mayada0bahij-technion.git>
2021-06-13 00:00:21 +0300

    test 5

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -12,61 +12,46 @@ export function SignupForm(props) {
     }
 
     return (
-        <BoxContainer>
-            <Form
-                onSubmit={onSubmit}
-                validate={values => {
-                    const errors = {};
-                    if (!values.username) {
-                        errors.username = "Required";
-                    }
-                    if (!values.password && values.username === "test") {
-                        errors.password = "Required";
-                    }
-                    return errors;
-                }}>
-                <div>
-                    <img align="left" src={img} alt="" width="200" height="200"/>
-                </div>
-                <br/>
-                <h1>הרשמה</h1>
-                <div>
-                    <input className="placeholder" type="text" required placeholder="שם פרטי"/>
-                </div>
-                <br/>
-                <div>
-                    <input className="placeholder" type="text" required placeholder="שם משפחה"/>
-                </div>
-                <br/>
-                <div>
-                    <input className="placeholder" type="number" required placeholder="מספר טלפון"/>
-                </div>
-                <br/>
-                <div>
-                    <input className="placeholder" type="email" required placeholder="אמייל"/>
-                </div>
-                <br/>
-                <div>
-                    <input className="placeholder" type="text" required placeholder="בית הספר"/>
-                </div>
-                <br/>
-                <div>
-                    <input className="placeholder" type="text" required placeholder="שם משתמש"/>
-                </div>
-                <br/>
-                <div>
-                    <input className="placeholder" type="text" required placeholder="סיסמה"/>
-                </div>
-                <div>
-                    <input className="placeholder" type="text" required placeholder=" אימות סיסמה"/>
-                </div>
-                <br/>
-                <div>
-                    <button className="buttonStyle">
-                        הירשם
-                    </button>
-                </div>
-            </form>
+        <div className="app-com">
+            <div>
+                <img align="left" src={img} alt="" width="200" height="200"/>
+            </div>
+            <br/>
+            <h1>הרשמה</h1>
+            <div>
+                <input className="placeholder" type="text" required placeholder="שם פרטי"/>
+            </div>
+            <br/>
+            <div>
+                <input className="placeholder" type="text" required placeholder="שם משפחה"/>
+            </div>
+            <br/>
+            <div>
+                <input className="placeholder" type="number" required placeholder="מספר טלפון"/>
+            </div>
+            <br/>
+            <div>
+                <input className="placeholder" type="email" required placeholder="אמייל"/>
+            </div>
+            <br/>
+            <div>
+                <input className="placeholder" type="text" required placeholder="בית הספר"/>
+            </div>
+            <br/>
+            <div>
+                <input className="placeholder" type="text" required placeholder="שם משתמש"/>
+            </div>
+            <br/>
+            <div>
+                <input className="placeholder" type="text" required placeholder="סיסמה"/>
+            </div>
+            <div>
+                <input className="placeholder" type="text" required placeholder=" אימות סיסמה"/>
+            </div>
+            <br/>
+            <div>
+                <button className="buttonStyle">הירשם</button>
+            </div>
         </div>
     )
 }
