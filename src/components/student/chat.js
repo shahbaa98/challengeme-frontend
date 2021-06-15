48ba768339500121a4792a0a947e8125ac94be9d
fredifin-git <fredifin@gmail.com>
2021-06-15 15:33:30 +0300

    Revert "."
    This reverts commit 9aac61a7b5c90cfacf3e0d15ab55f26528c8a56f.

--- src/components/student/chat.js
+++ src/components/student/chat.js
@@ -1,80 +1,23 @@
+import 'react-chatbox-component/dist/style.css';
+import {ChatBox} from 'react-chatbox-component';
+import avatar from '../imgs/chicken2.png'
 
-import React, { Component } from "react";
-import {
-  Chat,
-  Channel,
-  ChannelHeader,
-  Thread,
-  Window
-} from "stream-chat-react";
-import { MessageList, MessageInput } from "stream-chat-react";
-import { StreamChat } from "stream-chat";
-
-import "stream-chat-react/dist/css/index.css";
-class chat extends Component {
-  constructor(props) {
-    super(props);
-    this.client = new StreamChat("<1129032>");
-
-    this.client.setUser(
-      {
-        id: "cool-sky-9",
-        name: "Cool Sky",
-        image: "https://getstream.io/random_svg/?id=cool-sky-9&name=Cool+sky"
+const chat = () => {
+  const messages = [
+    {
+      "text": "שלום מורה, אפשר שאלה?",
+      "id": "1",
+      "sender": {
+        "name": "תלמיד",
+        "uid": "user1",
+        "avatar": avatar,
       },
-      localStorage.getItem("token")
-    );
-
-    this.channel = this.client.channel("messaging", "godevs", {
-      image:
-        "https://cdn.chrisshort.net/testing-certificate-chains-in-go/GOPHER_MIC_DROP.png",
-      name: "Talk about Go"
-    });
-  }
-
-  render() {
+    },
+  ]
     return (
-      <Chat client={this.client} theme={"messaging light"}>
-        <Channel channel={this.channel}>
-          <Window>
-            <ChannelHeader />
-            <MessageList />
-            <MessageInput />
-          </Window>
-          <Thread />
-        </Channel>
-      </Chat>
-    );
-  }
+      <ChatBox
+      messages={messages}
+    />
+      )
 }
-
-export default chat;
-
-
-
-
-
-
-// import 'react-chatbox-component/dist/style.css';
-// import {ChatBox} from 'react-chatbox-component';
-// import avatar from '../imgs/chicken2.png'
-
-// const chat = () => {
-//   const messages = [
-//     {
-//       "text": "שלום מורה, אפשר שאלה?",
-//       "id": "1",
-//       "sender": {
-//         "name": "תלמיד",
-//         "uid": "user1",
-//         "avatar": avatar,
-//       },
-//     },
-//   ]
-//     return (
-//       <ChatBox
-//       messages={messages}
-//     />
-//       )
-// }
-// export default chat
+export default chat
