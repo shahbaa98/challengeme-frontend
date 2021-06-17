bf78a872477f8180eb2cf7a74d36a12a671e4798
mayada0bahij <https://dbcourse@mayada0bahij-technion.scm.azurewebsites.net:443/mayada0bahij-technion.git>
2021-06-17 15:44:21 +0300

    newclass

--- src/components/Teacher/Classes.js
+++ src/components/Teacher/Classes.js
@@ -22,36 +22,9 @@ const Classes = () => {
                 </div>
             </div>
             <h1>הכיתות שלי</h1>
-            <br/><br/>
-            <div className="classes">
-                <div className="myClasses">הכיתות שלי</div>
-                <div className="col-12 justify-content-center">
-                    <div className="row mp0 flex-container containerClasses">
-                        <div className="classNameHome classStudentHome" dir="rtl">
-                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24"
-                                 className="closeIcon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
-                            </svg>
-                            <span className="verticalMiddle"> כיתה יא' 1</span></div>
-                        <div className="classNameHome classStudentHome" dir="rtl">
-                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24"
-                                 className="closeIcon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
-                            </svg>
-                            <span className="verticalMiddle"> כיתה יא' 2</span></div>
-                        <div className="classNameHome classStudentHome" dir="rtl">
-                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24"
-                                 className="closeIcon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
-                            </svg>
-                            <span className="verticalMiddle"> כיתה י' 1</span></div>
-                    </div>
-                </div>
-            </div>
+
         </div>
+
     )
 }
 export default Classes
