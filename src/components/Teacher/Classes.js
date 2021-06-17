a9bfc8f325dc547cb5161a2b8006592d6b9db2ab
mayada0bahij <https://dbcourse@mayada0bahij-technion.scm.azurewebsites.net:443/mayada0bahij-technion.git>
2021-06-17 14:52:52 +0300

    newclass

--- src/components/Teacher/Classes.js
+++ src/components/Teacher/Classes.js
@@ -1,11 +1,91 @@
+import {useHistory} from "react-router-dom";
+import {RiLogoutBoxLine} from "react-icons/ri";
+import img from '../imgs/image3.png'
+import '../style.css'
+import React, { useReducer,useState } from "react";
+import classes from "../imgs/class-icon.jpg";
+import newclass from "../imgs/add.png"
+
+
 
 const Classes = () => {
-    return (
-        <div>
-            <h1>
-                heyyyyyy
-            </h1>
+    const history = useHistory();
+    const logout = () => {
+        history.push('./');
+    }
+    const [formData, setFormData] = useReducer(formReducer, {});
+    const [submitting, setSubmitting] = useState(false);
+    const handleSubmit = event => {
+        event.preventDefault();
+        setSubmitting(true);
 
-        </div>)
+        setTimeout(() => {
+            setSubmitting(false);
+            setFormData({
+                reset: true
+            })
+        }, 3000)
+    }
+    //pulls the data from event.target
+    const handleChange = event => {
+        setFormData({
+            name: event.target.name,
+            value: event.target.value,
+        });
+    }
+    return (
+        <div className="app-com">
+            <div>
+                <div align ="left" onClick={()=> {history.push('/TeacherProfile')}}>
+                    &lt;  אחורה
+                </div>
+            </div>
+            <h1>הכיתות שלי</h1>
+            {submitting &&
+            <div>
+                You are submitting the following:
+                <ul>
+                    {Object.entries(formData).map(([name, value]) => (
+                        <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+                    ))}
+                </ul>
+            </div>
+            }
+            <br/><br/>
+            <div className="classes">
+                <div className="myClasses">הכיתות שלי</div>
+                <div className="col-12 justify-content-center">
+                    <div className="row mp0 flex-container containerClasses">
+                        <div className="classNameHome classStudentHome" dir="rtl">
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24"
+                                 className="closeIcon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+                            </svg>
+                            <span className="verticalMiddle"> כיתה יא' 1</span></div>
+                        <div className="classNameHome classStudentHome" dir="rtl">
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24"
+                                 className="closeIcon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+                            </svg>
+                            <span className="verticalMiddle"> כיתה יא' 2</span></div>
+                        <div className="classNameHome classStudentHome" dir="rtl">
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24"
+                                 className="closeIcon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+                            </svg>
+                            <span className="verticalMiddle"> כיתה י' 1</span></div>
+                    </div>
+                </div>
+            </div>
+            <div className="Newclass">
+                <div >שייך לכיתה חדשה</div>
+                <img     width= '200px' height='180px' src = {newclass} onClick={()=> {history.push('/classes')}}/>
+                <br/>
+            </div>
+        </div>
+    )
 }
 export default Classes
---
+++ src/components/Teacher/newclass.js
@@ -0,0 +1,91 @@
+import {useHistory} from "react-router-dom";
+import {RiLogoutBoxLine} from "react-icons/ri";
+import img from '../imgs/image3.png'
+import '../style.css'
+import React, { useReducer,useState } from "react";
+import classes from "../imgs/class-icon.jpg";
+import newclass from "../imgs/add.png"
+
+
+const formReducer = (state, event) => {
+    if(event.reset) {
+        return {
+            classID: '',
+            className: '',
+            teachername: '',
+        }
+    }
+    return {
+        ...state,
+        [event.name]: event.value
+    }
+}
+const Newclass = () => {
+    const history = useHistory();
+    const logout = () => {
+        history.push('./');
+    }
+    const [formData, setFormData] = useReducer(formReducer, {});
+    const [submitting, setSubmitting] = useState(false);
+    const handleSubmit = event => {
+        event.preventDefault();
+        setSubmitting(true);
+
+        setTimeout(() => {
+            setSubmitting(false);
+            setFormData({
+                reset: true
+            })
+        }, 3000)
+    }
+    //pulls the data from event.target
+    const handleChange = event => {
+        setFormData({
+            name: event.target.name,
+            value: event.target.value,
+        });
+    }
+    return (
+        <div className="app-com">
+            <div>
+                <div align ="left" onClick={()=> {history.push('/TeacherProfile')}}>
+                    &lt;  אחורה
+                </div>
+            </div>
+            <h1>הוספת כיתה חדשה </h1>
+            {submitting &&
+            <div>
+                You are submitting the following:
+                <ul>
+                    {Object.entries(formData).map(([name, value]) => (
+                        <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+                    ))}
+                </ul>
+            </div>
+            }
+            <br/><br/>
+            <form onSubmit={handleSubmit} dir="rtl"  >
+                <fieldset disabled={submitting}>
+                    <label className="label">
+                        <p>מספר כיתה:</p>
+                        <input className="box" type="number" name="classID" onChange={handleChange}  value={formData.classID || ''}/>
+                    </label>
+                    <br/>
+                    <label className="label">
+                        <p>שם כיתה:</p>
+                        <input className="box" type="text" name="className" onChange={handleChange} value={formData.className || ''}/>
+                    </label>
+                    <br/>
+                    <label className="label">
+                        <p>שם מורה אחראי:</p>
+                        <input className="box" type="text" name="teachername" onChange={handleChange}  value={formData.teachername || ''}/>
+                    </label>
+                    <br/>
+                </fieldset>
+                <br/>
+                <button type="submit" disabled={submitting}>הוספה</button>
+            </form>
+        </div>
+    )
+}
+export default Newclass
