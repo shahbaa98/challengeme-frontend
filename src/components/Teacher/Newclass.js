2db15844580dc4377d23324176b70ff2a6c32351
mayada0bahij <https://dbcourse@mayada0bahij-technion.scm.azurewebsites.net:443/mayada0bahij-technion.git>
2021-06-19 15:22:42 +0300

    class

--- src/components/Teacher/Newclass.js
+++ src/components/Teacher/Newclass.js
@@ -33,7 +33,7 @@ const Newclass = () => {
 
         };
 
-        fetch('http://127.0.0.1:8000/api/v1/users/auth/register/', {
+        fetch('http://127.0.0.1:8000/api/v1/Classes', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
