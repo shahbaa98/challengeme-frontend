b1ec093021fb4095737e7ec52381fa64908bdf90
mayada0bahij <https://dbcourse@mayada0bahij-technion.scm.azurewebsites.net:443/mayada0bahij-technion.git>
2021-06-26 15:03:23 +0300

    1

--- src/components/Teacher/StudentReportProfile.js
+++ src/components/Teacher/StudentReportProfile.js
@@ -28,13 +28,6 @@ const StudentReportProfile = () => {
                 </label>
                 <br />
             </div>
-                <div align="right" dir="rtl">
-                    <label for="fileImgStudent" className="lableImg">
-                        <img width="200" height="200" src={profile} />
-                    </label>
-                    <br />
-                </div>
-
             <div className="Report">
                 <div >דווחות</div>
                 <img width='200px' height='180px' src={Report} onClick={() => { history.push('/Report') }} />
